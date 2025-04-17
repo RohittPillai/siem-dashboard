56269d6b1230fe704c11bada9c7812a4303e2caf
RohittPillai <rohitpillai981@gmail.com>
2025-04-17 18:04:36 -0400

    Logs viewable through /logs

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,11 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client['siem']
 logs_collection = db['logs']
 
+def serialize_doc(doc):
+    doc["_id"] = str(doc["_id"])  # Convert ObjectId to string
+    return doc
+
+
 @app.route('/')
 def home():
     return "🚀 SIEM Flask API is running!"
@@ -24,6 +29,13 @@ def ingest_log():
         "status": "Log saved to MongoDB",
         "log_id": str(result.inserted_id)  # ✅ convert ObjectId to string
     }), 200
+
+@app.route('/logs', methods=['GET'])
+def get_logs():
+    logs = list(logs_collection.find())
+    serialized_logs = [serialize_doc(log) for log in logs]
+    return jsonify(serialized_logs), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
